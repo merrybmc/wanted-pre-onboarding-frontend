70e6d93a076aba93823040b717cef538bd1e87c8
Byeong Min Cho <merrybmc@gmail.com>
2023-02-06 19:37:33 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>원티드</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/router/Router.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+export default function router() {
+  return <div>router</div>;
+}
